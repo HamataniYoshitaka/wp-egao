3ab04ecd247dc3d7893e7bc8f38419ef4cb8a603
Hamatani <y.hamatani@pulseanddecibels.jp>
2016-09-22 22:12:48 +0900

    ひとのこと、院内紹介

---
+++ page-dentist.php
@@ -0,0 +1,77 @@
+<?
+/**
+* Template Name: 人のこと
+*/
+get_header();
+require_once(dirname(__FILE__) . '/parts/nav-pc.php');
+?>
+<div class="fullscreen" style="background-image: url(<? bloginfo('template_directory') ?>/compass/images/dummy/bg1.jpg);">
+    <div class="middle">
+        <div class="text-center" style="margin-top: -100px; line-height: 5;">
+            <img src="<? bloginfo('template_directory') ?>/compass/images/staff-title.png" srcset="<? bloginfo('template_directory') ?>/compass/images/staff-title@2x.png 2x"><br>
+            <img src="<? bloginfo('template_directory') ?>/compass/images/wave-white.png" srcset="<? bloginfo('template_directory') ?>/compass/images/wave-white@2x.png 2x">
+            <h4 class="bold tracking-1">人のこと</h4>
+        </div>
+    </div>
+</div>
+
+<div class="bg-white">
+    <div class="container" style="padding-top: 60px; padding-bottom: 60px;">
+        <h1 class="text-center tracking-2 bold">DENTIST</h1>
+        <br>
+        <h5 class="text-center bold tracking-1">医師紹介</h5>
+        <br>
+        <p class="text-center"><img src="<? bloginfo('template_directory') ?>/compass/images/half-balls.png" srcset="<? bloginfo('template_directory') ?>/compass/images/half-balls@2x.png 2x"></p>
+
+        <div class="row" style="margin-top: 60px;">
+            <div class="col-sm-6">
+            </div>
+            <div class="col-sm-6">
+                <h3><small>院長</small>&nbsp;生野 啓一郎</h3>
+                <p class="line-height-2">
+                    1982年生まれ　34歳 / 2001年　県立千葉高校卒業 / 2008年　北海道大学歯学部卒業 / 2009年　北海道大学歯科矯正学教室入局 / 2013年　北海道大学大学院歯学研究科卒業（博士） / 2014年　日本矯正歯科学会認定医取得 / 2016年　「矯正歯科egao」開設
+                </p>
+                <br>
+                <h5 class="bold">主な記録</h5>
+                <p class="line-height-2">
+                    フルマラソン記録 : 3時間46分26秒(ネットタイム)  / 献血回数: 20回 / 羊蹄山登頂経験あり
+                </p>
+                <br>
+                <h5 class="bold">自己紹介</h5>
+                <p class="line-height-2">
+                    歯並びがきれいになってきたことを喜んでくれる患者さんの笑顔をみた時、このうえない喜びを感じます。
+                </p>
+            </div>
+        </div><!-- /.row -->
+
+        <div class="row" style="margin-top: 60px;">
+            <div class="col-sm-6">
+            </div>
+            <div class="col-sm-6">
+                <h3><small>院長</small>&nbsp;生野 さやか</h3>
+                <p class="line-height-2">
+                    1983年生まれ　33歳 / 2002年　札幌北高校卒業 /  2008年　北海道大学歯学部卒業 / 2008年　北海道病院歯科診療センター研修医、第２補綴教室入局 / 2009年〜2016年　札幌市内や千葉県柏市にて勤務医 / 2016年　「矯正歯科egao」副院長 / 顎咬合学会　咬み合わせ認定医
+                </p>
+                <br>
+                <h5 class="bold">自己紹介</h5>
+                <p class="line-height-2">
+                    もうすぐ２歳の男の子のママです。ついママ目線で子供と接してしまいます。<br>
+                    子供とママに寄り添った、優しくあたたかいケア、キュアを心がけています。<br>
+                    パン・お菓子づくりにはちょっと凝っていますのでいつかごちそうします!
+                </p>
+            </div>
+        </div><!-- /.row -->
+    </div><!-- /.container -->
+</div><!-- /.bg-white -->
+
+<div class="container" style="padding-top: 60px; padding-bottom: 60px;">
+    <h1 class="text-center tracking-2 bold">STAFF</h1>
+    <br>
+    <h5 class="text-center bold tracking-1">スタッフ紹介</h5>
+    <br>
+    <p class="text-center"><img src="<? bloginfo('template_directory') ?>/compass/images/half-balls.png" srcset="<? bloginfo('template_directory') ?>/compass/images/half-balls@2x.png 2x"></p>
+</div><!-- /.container -->
+
+
+
+<? get_footer(); ?>
---
+++ page-introduction.php
@@ -0,0 +1,43 @@
+<?
+/**
+* Template Name: 院内紹介
+*/
+get_header();
+require_once(dirname(__FILE__) . '/parts/nav-pc.php');
+?>
+<div class="fullscreen" style="background-image: url(<? bloginfo('template_directory') ?>/compass/images/dummy/bg1.jpg);">
+    <div class="middle">
+        <div class="text-center" style="margin-top: -100px; line-height: 5;">
+            <img src="<? bloginfo('template_directory') ?>/compass/images/introduction-title.png" srcset="<? bloginfo('template_directory') ?>/compass/images/introduction-title@2x.png 2x"><br>
+            <img src="<? bloginfo('template_directory') ?>/compass/images/wave-white.png" srcset="<? bloginfo('template_directory') ?>/compass/images/wave-white@2x.png 2x">
+            <h4 class="bold tracking-1">院内紹介</h4>
+        </div>
+    </div>
+</div>
+
+<div class="bg-white">
+    <div class="container" style="padding-top: 60px; padding-bottom: 60px;">
+        <h1 class="text-center tracking-2 bold">ROOM</h1>
+        <br>
+        <h5 class="text-center bold tracking-1">えがお矯正歯科の院内紹介</h5>
+        <br>
+        <p class="text-center"><img src="<? bloginfo('template_directory') ?>/compass/images/half-balls.png" srcset="<? bloginfo('template_directory') ?>/compass/images/half-balls@2x.png 2x"></p>
+        <p class="text-center bold">「歯医者らしくない空間」「くつろげる空間」「ブルックリンスタイル」をテーマに空間を作っています。</p>
+
+
+    </div><!-- /.container -->
+</div><!-- /.bg-white -->
+
+<div class="container" style="padding-top: 60px; padding-bottom: 60px;">
+    <h1 class="text-center tracking-2 bold">MEDICAL EQUIPMENT</h1>
+    <br>
+    <h5 class="text-center bold tracking-1">えがお矯正歯科の医療器具</h5>
+    <br>
+    <p class="text-center"><img src="<? bloginfo('template_directory') ?>/compass/images/half-balls.png" srcset="<? bloginfo('template_directory') ?>/compass/images/half-balls@2x.png 2x"></p>
+    <p class="text-center bold">ダミーです。当院では滅菌にこだわっています。テキストテキストテイスト</p>
+
+</div><!-- /.container -->
+
+
+
+<? get_footer(); ?>
