0afa94b210a595fa6511699d6950641de147568c
Hamatani <y.hamatani@pulseanddecibels.jp>
2016-09-22 22:24:55 +0900

    診療について、途中

---
+++ page-treatment.php
@@ -0,0 +1,61 @@
+<?
+/**
+* Template Name: 診療について
+*/
+get_header();
+require_once(dirname(__FILE__) . '/parts/nav-pc.php');
+?>
+<div class="fullscreen" style="background-image: url(<? bloginfo('template_directory') ?>/compass/images/dummy/bg1.jpg);">
+    <div class="middle">
+        <div class="text-center" style="margin-top: -100px; line-height: 5;">
+            <img src="<? bloginfo('template_directory') ?>/compass/images/treatment-title.png" srcset="<? bloginfo('template_directory') ?>/compass/images/treatment-title@2x.png 2x"><br>
+            <img src="<? bloginfo('template_directory') ?>/compass/images/wave-white.png" srcset="<? bloginfo('template_directory') ?>/compass/images/wave-white@2x.png 2x">
+            <h4 class="bold tracking-1">診療について</h4>
+        </div>
+    </div>
+</div>
+
+<div class="bg-white">
+    <div class="container" style="padding-top: 60px; padding-bottom: 60px;">
+        <h1 class="text-center tracking-2 bold">TREATMENT CONTENT</h1>
+        <br>
+        <h5 class="text-center bold tracking-1">治療内容</h5>
+        <br>
+        <p class="text-center"><img src="<? bloginfo('template_directory') ?>/compass/images/half-balls.png" srcset="<? bloginfo('template_directory') ?>/compass/images/half-balls@2x.png 2x"></p>
+
+        <div class="row" style="margin-top: 60px;">
+            <div class="col-sm-6 col-md-5 col-md-offset-1">
+                <h3 class="text-center bold">矯正治療</h3>
+                <br>
+                <p class="line-height-2">
+                    ダミーです。歯の裏側に矯正装置をつけて治療を行うのが「裏側矯正」です。最大のメリットは、表からは装置が見えないことです。周囲の目を気にすることなく治療ができるため、装置が目立つからと矯正治療に踏み出せなかった方も安心して始められます。裏側矯正は高い技術が必要なため、すべての矯正歯科で対応できるまでには至っていません。しかし当院では、経験を積んだ医師が患者さんのお口の状況やご希望に沿って高い技術で裏側矯正を行っています。裏側矯正にも対応できる当院ならば、装置が目立つ上の歯は裏側矯正にし、舌が当たる下の歯は表側矯正をすることもでき、治療の選択肢が広がります。まずは気軽にご相談ください
+                </p>
+            </div>
+            <div class="col-sm-6 col-md-5">
+                <h3 class="text-center bold">小児予防歯科</h3>
+                <br>
+                <p class="line-height-2">
+                    ダミーです。歯の裏側に矯正装置をつけて治療を行うのが「裏側矯正」です。最大のメリットは、表からは装置が見えないことです。周囲の目を気にすることなく治療ができるため、装置が目立つからと矯正治療に踏み出せなかった方も安心して始められます。裏側矯正は高い技術が必要なため、すべての矯正歯科で対応できるまでには至っていません。しかし当院では、経験を積んだ医師が患者さんのお口の状況やご希望に沿って高い技術で裏側矯正を行っています。裏側矯正にも対応できる当院ならば、装置が目立つ上の歯は裏側矯正にし、舌が当たる下の歯は表側矯正をすることもでき、治療の選択肢が広がります。まずは気軽にご相談ください
+                </p>
+            </div>
+        </div><!-- /.row -->
+
+    </div><!-- /.container -->
+</div><!-- /.bg-white -->
+
+<div class="container" style="padding-top: 60px; padding-bottom: 60px;">
+    <h1 class="text-center tracking-2 bold">STEP</h1>
+    <br>
+    <h5 class="text-center bold tracking-1">えがお矯正歯科の治療の進め方</h5>
+    <br>
+    <p class="text-center"><img src="<? bloginfo('template_directory') ?>/compass/images/half-balls.png" srcset="<? bloginfo('template_directory') ?>/compass/images/half-balls@2x.png 2x"></p>
+
+    <div class="row" style="margin-top: 60px;">
+        
+    </div><!-- /.row -->
+
+</div><!-- /.container -->
+
+
+
+<? get_footer(); ?>
