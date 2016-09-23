f803927b9f0eb412320de9b5fde92ee3749a6bf7
Hamatani <y.hamatani@pulseanddecibels.jp>
2016-09-23 11:41:33 +0900

    cost.php 半分

---
+++ page-cost.php
@@ -0,0 +1,231 @@
+<?
+/**
+* Template Name: 矯正治療費について
+*/
+get_header();
+require_once(dirname(__FILE__) . '/parts/nav-pc.php');
+?>
+<div class="fullscreen" style="background-image: url(<? bloginfo('template_directory') ?>/compass/images/dummy/bg1.jpg);">
+    <div class="middle">
+        <div class="text-center" style="margin-top: -100px; line-height: 5;">
+            <img src="<? bloginfo('template_directory') ?>/compass/images/cost-title.png"><br>
+            <img src="<? bloginfo('template_directory') ?>/compass/images/wave-white.png" srcset="<? bloginfo('template_directory') ?>/compass/images/wave-white@2x.png 2x">
+            <h4 class="bold tracking-1">矯正治療費について</h4>
+        </div>
+    </div>
+</div>
+
+<div class="bg-white">
+    <div class="container" style="padding-top: 60px; padding-bottom: 60px;">
+        <h1 class="text-center tracking-2 bold">STEP &amp; FEE</h1>
+        <br>
+        <h5 class="text-center bold tracking-1">治療のステップと費用</h5>
+        <br>
+        <p class="text-center"><img src="<? bloginfo('template_directory') ?>/compass/images/half-balls.png" srcset="<? bloginfo('template_directory') ?>/compass/images/half-balls@2x.png 2x"></p>
+
+        <h3 class="text-center bold" style="margin-top: 100px;">子供（永久歯列前）</h3>
+
+        <div class="row" style="margin-top: 100px;">
+            <div class="col-sm-6 col-sm-offset-1" style="position: relative;">
+                <img class="cost-circle" src="<? bloginfo('template_directory') ?>/compass/images/cost-circle.png" srcset="<? bloginfo('template_directory') ?>/compass/images/cost-circle@2x.png 2x">
+                <h3 class="bold tracking-1">第一期治療&emsp;<small class="color-default bold">-永久歯列前の矯正治療-</small></h3>
+                <br>
+                <p class="line-height-2">
+                    全ての歯が生え変わる12歳前後までの治療です。<br>
+                    歯の萌え変わりと顎の成長をコントロールし、<br>上下の顎のバランスを整えていきます。
+                </p>
+                <br>
+                <h4 class="bold tracking-1"><span class="cost-borderd-box roboto-condensed">100,000<small class="color-default bold">円</small> ~ 300,000<small class="color-default bold">円</small></span></h4>
+            </div>
+            <div class="col-sm-5">
+                <div class="cost-box">
+                    <p class="line-height-2 text-center">
+                        4週に1度、来院して頂き調整を行います。<br>
+                        その際に毎回調整料がかかります。<br>
+                        歯の萌え変わりを待つ間を<br>
+                        開けることもあります。
+                    </p>
+                    <br>
+                    <h4 class="bold tracking-1 text-center"><span class="cost-borderd-box roboto-condensed">調節料&nbsp;5,000<small class="color-default bold">円</small></span></h4>
+                </div>
+            </div>
+        </div><!-- /.row -->
+
+        <div class="row" style="margin-top: 100px;">
+            <div class="col-sm-6 col-sm-offset-1" style="position: relative;">
+                <img class="cost-circle" src="<? bloginfo('template_directory') ?>/compass/images/cost-circle.png" srcset="<? bloginfo('template_directory') ?>/compass/images/cost-circle@2x.png 2x">
+                <h3 class="bold tracking-1">追加検査&emsp;<small class="color-default bold">-第二期治療開始時に発生-</small></h3>
+                <br>
+                <h4 class="bold tracking-1"><span class="cost-borderd-box roboto-condensed">30,000<small class="color-default bold">円</small></span></h4>
+            </div>
+        </div><!-- /.row -->
+
+        <div class="row" style="margin-top: 100px;">
+            <div class="col-sm-6 col-sm-offset-1" style="position: relative;">
+                <img class="cost-circle" src="<? bloginfo('template_directory') ?>/compass/images/cost-circle.png" srcset="<? bloginfo('template_directory') ?>/compass/images/cost-circle@2x.png 2x">
+                <h3 class="bold tracking-1">第二期治療&nbsp;<small class="color-default bold" style="letter-spacing: 0;">-第一期治療完了後の永久歯列前の矯正治療-</small></h3>
+                <br>
+                <p class="line-height-2">
+                    全ての歯が永久歯に萌え変わった後の治療です。<br>
+                    歯列の凸凹等を解消していきます。
+                </p>
+                <br>
+                <h4 class="bold tracking-1"><span class="cost-borderd-box roboto-condensed">30,000<small class="color-default bold">円</small></span></h4>
+            </div>
+            <div class="col-sm-5">
+                <div class="cost-box">
+                    <p class="line-height-2 text-center">
+                        4週に1度、来院して頂き調整を行います。<br>
+                        その際に毎回調整料がかかります。
+                    </p>
+                    <br>
+                    <h4 class="bold tracking-1 text-center"><span class="cost-borderd-box roboto-condensed">調節料&nbsp;5,000<small class="color-default bold">円</small></span></h4>
+                </div>
+            </div>
+        </div><!-- /.row -->
+
+        <div class="row" style="margin-top: 100px;">
+            <div class="col-sm-6 col-sm-offset-1" style="position: relative;">
+                <img class="cost-circle" src="<? bloginfo('template_directory') ?>/compass/images/cost-circle.png" srcset="<? bloginfo('template_directory') ?>/compass/images/cost-circle@2x.png 2x">
+                <h3 class="bold tracking-1">最終検査</h3>
+                <br>
+                <h4 class="bold tracking-1"><span class="cost-borderd-box roboto-condensed">20,000<small class="color-default bold">円</small></span></h4>
+            </div>
+        </div><!-- /.row -->
+
+        <div class="row" style="margin-top: 100px;">
+            <div class="col-sm-6 col-sm-offset-1" style="position: relative;">
+                <img class="cost-circle" src="<? bloginfo('template_directory') ?>/compass/images/cost-circle.png" srcset="<? bloginfo('template_directory') ?>/compass/images/cost-circle@2x.png 2x">
+                <h3 class="bold tracking-1">保定、経過観察</h3>
+                <br>
+                <p class="line-height-2">
+                    キレイになった歯並びが（咬合）が元に戻らないように保定装置を使用して頂き、その状態を維持・経過観察を行っていきます。
+                </p>
+            </div>
+            <div class="col-sm-5">
+                <div class="cost-box">
+                    <p class="line-height-2 text-center">
+                        治療が終わった後、<br>
+                        3ヶ月〜半年毎に定期観察をさせて頂きます。<br>
+                        その際に毎回観察料がかかります。
+                    </p>
+                    <br>
+                    <h4 class="bold tracking-1 text-center"><span class="cost-borderd-box roboto-condensed">観察料&nbsp;3,000<small class="color-default bold">円</small></span></h4>
+                </div>
+            </div>
+        </div><!-- /.row -->
+
+        <h3 class="text-center bold" style="margin-top: 100px;">成人（永久歯列期）</h3>
+
+        <div class="row" style="margin-top: 100px;">
+            <div class="col-sm-6 col-sm-offset-1" style="position: relative;">
+                <img class="cost-circle" src="<? bloginfo('template_directory') ?>/compass/images/cost-circle.png" srcset="<? bloginfo('template_directory') ?>/compass/images/cost-circle@2x.png 2x">
+                <h3 class="bold tracking-1">顎機能検査&emsp;<small class="color-default bold">-成人は必ず行う検査です-</small></h3>
+                <br>
+                <h4 class="bold tracking-1"><span class="cost-borderd-box roboto-condensed">17,000<small class="color-default bold">円</small></span></h4>
+            </div>
+        </div><!-- /.row -->
+
+        <div class="row" style="margin-top: 100px;">
+            <div class="col-sm-6 col-sm-offset-1" style="position: relative;">
+                <img class="cost-circle" src="<? bloginfo('template_directory') ?>/compass/images/cost-circle.png" srcset="<? bloginfo('template_directory') ?>/compass/images/cost-circle@2x.png 2x">
+                <h3 class="bold tracking-1">永久歯列の矯正治療&emsp;<small class="color-default bold">-装置が選べます-</small></h3>
+                <br>
+                <p class="line-height-2">
+                    全ての歯が永久歯に生え変わった後の治療です。<br>
+                    歯列の凸凹等を解消していきます。
+                </p>
+                <br>
+                <h4 class="bold tracking-1"><span class="cost-borderd-box roboto-condensed">100,000<small class="color-default bold">円</small> ~ 850,000<small class="color-default bold">円</small></span></h4>
+            </div>
+            <div class="col-sm-5">
+                <div class="cost-box">
+                    <p class="line-height-2 text-center">
+                        4週に1度、来院して頂き調整を行います。<br>
+                        その際に毎回調整料がかかります。<br>
+                        使用する装置により調整料は異なります。
+                    </p>
+                    <br>
+                    <div class="cost-borderd-box text-center" style="width: 260px; margin: 0 auto;">
+                        <h5 class="bold">調節料</h5>
+                        <p class="line-height-2 bold">
+                            クリアブランケット… 5,000円<br>
+                            舌側（リンガル） …8,000円<br>
+                            インビザライン… 5,000円
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div><!-- /.row -->
+
+        <div class="row" style="margin-top: 30px;">
+            <div class="col-sm-6 col-sm-offset-1" style="position: relative;">
+                <img class="cost-circle" src="<? bloginfo('template_directory') ?>/compass/images/cost-circle.png" srcset="<? bloginfo('template_directory') ?>/compass/images/cost-circle@2x.png 2x">
+                <h3 class="bold tracking-1">最終検査</h3>
+                <br>
+                <h4 class="bold tracking-1"><span class="cost-borderd-box roboto-condensed">20,000<small class="color-default bold">円</small></span></h4>
+            </div>
+        </div><!-- /.row -->
+
+        <div class="row" style="margin-top: 100px;">
+            <div class="col-sm-6 col-sm-offset-1" style="position: relative;">
+                <img class="cost-circle" src="<? bloginfo('template_directory') ?>/compass/images/cost-circle.png" srcset="<? bloginfo('template_directory') ?>/compass/images/cost-circle@2x.png 2x">
+                <h3 class="bold tracking-1">保定、経過観察</h3>
+                <br>
+                <p class="line-height-2">
+                    キレイになった歯並びが（咬合）が元に戻らないように保定装置を使用して頂き、その状態を維持・経過観察を行っていきます。
+                </p>
+            </div>
+            <div class="col-sm-5">
+                <div class="cost-box">
+                    <p class="line-height-2 text-center">
+                        治療が終わった後、<br>
+                        3ヶ月〜半年毎に定期観察をさせて頂きます。<br>
+                        その際に毎回観察料がかかります。
+                    </p>
+                    <br>
+                    <h4 class="bold tracking-1 text-center"><span class="cost-borderd-box roboto-condensed">観察料&nbsp;3,000<small class="color-default bold">円</small></span></h4>
+                </div>
+            </div>
+        </div><!-- /.row -->
+
+    </div><!-- /.container -->
+</div><!-- /.bg-white -->
+
+<div class="container" style="padding-top: 60px; padding-bottom: 60px;">
+    <h1 class="text-center tracking-2 bold">STEP</h1>
+    <br>
+    <h5 class="text-center bold tracking-1">えがお矯正歯科の治療の進め方</h5>
+    <br>
+    <p class="text-center"><img src="<? bloginfo('template_directory') ?>/compass/images/half-balls.png" srcset="<? bloginfo('template_directory') ?>/compass/images/half-balls@2x.png 2x"></p>
+
+    <div class="row" style="margin-top: 120px;">
+        <div class="col-sm-6 col-md-5">
+            <h3><span class="round-border-title tracking-1">初診矯正相談</span></h3>
+            <br>
+            <h4 class="bold">一人一人にあった治療方法を探します。</h4>
+            <p class="line-height-2">
+                矯正治療についておおまかな説明をします。<br>
+                患者さんの気になっていることや症状を聞き、それらについての矯正治療のおおまなかなお話をします。<br>
+                どのような細かい質問でもどんどん聞いて下さい。
+            </p>
+        </div>
+        <div class="col-sm-6 col-md-5 col-md-offset-2">
+            <div style="position: relative;">
+                <img class="img-responsive" src="http://placehold.it/400x260">
+                <div style="position: absolute; top: -50px; width: 100%; left: 0; text-align: center">
+                    <div class="treatment-step">
+                        <h1 class="roboto-condensed">1</h1>
+                        <h6 class="bold">STEP</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- /.row -->
+
+
+</div><!-- /.container -->
+
+
+
+<? get_footer(); ?>
