fb86b6d69d613a8b709c07501eb0caa2fc5c447e
Hamatani <y.hamatani@pulseanddecibels.jp>
2016-09-23 10:26:21 +0900

    treatment.phpできてきた

--- page-treatment.php
+++ page-treatment.php
@@ -50,8 +50,102 @@ require_once(dirname(__FILE__) . '/parts/nav-pc.php');
     <br>
     <p class="text-center"><img src="<? bloginfo('template_directory') ?>/compass/images/half-balls.png" srcset="<? bloginfo('template_directory') ?>/compass/images/half-balls@2x.png 2x"></p>
 
-    <div class="row" style="margin-top: 60px;">
-        
+    <div class="row" style="margin-top: 120px;">
+        <div class="col-sm-6 col-md-5">
+            <h3><span class="round-border-title tracking-1">初診矯正相談</span></h3>
+            <br>
+            <h4 class="bold">一人一人にあった治療方法を探します。</h4>
+            <p class="line-height-2">
+                矯正治療についておおまかな説明をします。<br>
+                患者さんの気になっていることや症状を聞き、それらについての矯正治療のおおまなかなお話をします。<br>
+                どのような細かい質問でもどんどん聞いて下さい。
+            </p>
+        </div>
+        <div class="col-sm-6 col-md-5 col-md-offset-2">
+            <div style="position: relative;">
+                <img class="img-responsive" src="http://placehold.it/400x260">
+                <div style="position: absolute; top: -50px; width: 100%; left: 0; text-align: center">
+                    <div class="treatment-step">
+                        <h1 class="roboto-condensed">1</h1>
+                        <h6 class="bold">STEP</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- /.row -->
+
+    <div class="row" style="margin-top: 120px;">
+        <div class="col-sm-6">
+            <h3><span class="round-border-title tracking-1">精密検査</span></h3>
+            <br>
+            <h4 class="bold">丁寧に検査し、歯の現状を知ります。</h4>
+            <p class="line-height-2">
+                詳しい検査をします。診断そして治療計画作成に必要なレントゲン・歯型・口の中や顔の写真を採得します。<br>
+                時間は子供と成人では違いますが、検査項目が多く最低でも1時間程度は必要です。
+            </p>
+        </div>
+        <div class="col-sm-6 col-md-5 col-md-offset-1">
+            <div style="position: relative;">
+                <img class="img-responsive" src="http://placehold.it/400x260">
+                <div style="position: absolute; top: -50px; width: 100%; left: 0; text-align: center">
+                    <div class="treatment-step">
+                        <h1 class="roboto-condensed">2</h1>
+                        <h6 class="bold">STEP</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- /.row -->
+
+    <div class="row" style="margin-top: 120px;">
+        <div class="col-sm-6">
+            <h3><span class="round-border-title tracking-1">診断</span></h3>
+            <br>
+            <h4 class="bold">不安な事があればどんな小さなことでもご相談ください。</h4>
+            <p class="line-height-2">
+                治療について具体的な説明をします。<br>
+                患者さんの骨格パターンをコンピューターで分析した結果や、口元や歯並びの問題点についてお話します。
+                患者さんのご希望やご意見も治療方針に反映されますので、遠慮せずなんなりとお話ください。<br>
+                矯正治療を受けていただくかどうかの判断は、検査結果や治療計画を聞いた後で構いません。
+            </p>
+        </div>
+        <div class="col-sm-6 col-md-5 col-md-offset-1">
+            <div style="position: relative;">
+                <img class="img-responsive" src="http://placehold.it/400x260">
+                <div style="position: absolute; top: -50px; width: 100%; left: 0; text-align: center">
+                    <div class="treatment-step">
+                        <h1 class="roboto-condensed">3</h1>
+                        <h6 class="bold">STEP</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- /.row -->
+
+    <div class="row" style="margin-top: 120px;">
+        <div class="col-sm-6">
+            <h3><span class="round-border-title tracking-1">装置セット・治療開始</span></h3>
+            <br>
+            <h4 class="bold">笑顔ので暮らす治療のはじまりです。</h4>
+            <p class="line-height-2">
+                矯正装置をお口のなかに装着します。
+                装置により時間は違いますが、注意事項の説明や歯磨きの練習まで含めるとたいてい1時間程度は必要です。<br>
+                いよいよ矯正治療の始まりです。<br>
+                骨の中をゆっくりと歯が移動しますので、短い道のりとは言えませんが健康な歯並びが待っています。<br>
+                一緒にがんばりましょう。
+            </p>
+        </div>
+        <div class="col-sm-6 col-md-5 col-md-offset-1">
+            <div style="position: relative;">
+                <img class="img-responsive" src="http://placehold.it/400x260">
+                <div style="position: absolute; top: -50px; width: 100%; left: 0; text-align: center">
+                    <div class="treatment-step">
+                        <h1 class="roboto-condensed">4</h1>
+                        <h6 class="bold">STEP</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div><!-- /.row -->
 
 </div><!-- /.container -->
